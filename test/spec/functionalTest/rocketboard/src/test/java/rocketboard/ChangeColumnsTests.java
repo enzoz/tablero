4950c63e6b2e3b94322f269e453590f76429b642
Enzo Zuccolotto <enzozuccolotto@gmail.com>
2015-03-04 17:57:25 -0300

    Functional tests for adding and removing columns from board

---
+++ test/spec/functionalTest/rocketboard/src/test/java/rocketboard/ChangeColumnsTests.java
@@ -0,0 +1,51 @@
+package rocketboard;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ChangeColumnsTests extends AbstractRocketboardTests {
+
+    @Test
+    public void AddNewColumn() throws Exception {
+        rocketboardPage.waitingLoading();
+        rocketboardPage.openColumnsModal();
+        rocketboardPage.clickAddNewColumnLink();
+
+        WebElement newColumnInput = driver.findElement(By.cssSelector("ul.columns-container li:last-child input"));
+        newColumnInput.sendKeys("New Column");
+
+        rocketboardPage.saveColumnsChanges();
+        rocketboardPage.waitingLoading();
+
+        assertTrue(driver.findElement(By.cssSelector(".panel-heading.new-column-header")).isDisplayed());
+    }
+
+    @Test
+    public void RemoveColumn() throws Exception {
+        rocketboardPage.waitingLoading();
+        rocketboardPage.openColumnsModal();
+        rocketboardPage.clickAddNewColumnLink();
+
+        WebElement newColumnInput = driver.findElement(By.cssSelector("ul.columns-container li:last-child input"));
+        newColumnInput.sendKeys("New Column");
+
+        rocketboardPage.saveColumnsChanges();
+        rocketboardPage.waitingLoading();
+
+        rocketboardPage.openColumnsModal();
+        newColumnInput = driver.findElement(By.cssSelector("ul.columns-container li:last-child input"));
+        newColumnInput.sendKeys("");
+        rocketboardPage.saveColumnsChanges();
+        rocketboardPage.waitingLoading();
+
+        assertTrue(driver.findElement(By.cssSelector(".panel-heading.new-column-header")).isDisplayed());
+    }
+    
+}
